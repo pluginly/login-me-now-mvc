3c34311cdea96572ac2b273d704d07819690fd17
arafat-zip <powergenyt@gmail.com>
2025-05-07 08:50:32 +0600

    some file delete becouse of duplicate and some files edited for changing

--- routes/rest/magic-link.php
+++ routes/rest/magic-link.php
@@ -3,6 +3,6 @@
 use LoginMeNow\App\Http\MagicLinkController\MagicLinkController;
 use LoginMeNow\WpMVC\Routing\Route;
 
-include __DIR__ . '/admin-settings.php';
+
 
 Route::post( '/send-magic-link', [MagicLinkController::class, 'send_magic_link'] );
