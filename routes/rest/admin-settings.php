c411f246eb5e3868e4a7d118e55f85c9e6fe7546
arafat-zip <powergenyt@gmail.com>
2025-05-07 10:55:30 +0600

    magic link Route removed from admin setting

--- routes/rest/admin-settings.php
+++ routes/rest/admin-settings.php
@@ -1,11 +1,8 @@
 <?php
 
 use LoginMeNow\App\Http\Controllers\SettingsController;
-use LoginMeNow\App\Http\MagicLinkController\MagicLinkController;
-use LoginMeNow\App\Http\Controllers\Controller;
 use LoginMeNow\WpMVC\Routing\Route;
 
 Route::get( 'admin/settings', [SettingsController::class, 'index'], ['admin'] );
 Route::post( 'admin/settings/fields', [SettingsController::class, 'get_fields'], ['admin'] );
 Route::post( 'admin/settings/save', [LoginMeNow\App\http\Controllers\Controller::class, 'save'], ['admin'] );
-Route::post( '/send-magic-link', [MagicLinkController::class, 'send_magic_link'] );
