17992ce61bcd3acf9c9a0521fab7e01f9143b258
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2025-05-18 20:58:25 +0600

    removed ajax routes

--- routes/ajax/api.php
+++ routes/ajax/api.php
@@ -5,4 +5,8 @@ defined( 'ABSPATH' ) || exit;
 use LoginMeNow\App\Http\Controllers\BrowserTokenController;
 use LoginMeNow\WpMVC\Routing\Ajax;
 
-Ajax::get( 'login_me_now_browser_token_generate', [BrowserTokenController::class, 'save_token'] );
+Ajax::post( 'hide_save_to_browser_extension', [BrowserTokenController::class, 'hide_save_to_browser_extension'] );
+Ajax::post( 'browser_token_generate', [BrowserTokenController::class, 'browser_token_generate'] );
+Ajax::post( 'browser_tokens', [BrowserTokenController::class, 'browser_tokens'] );
+Ajax::post( 'browser_token_update_status', [BrowserTokenController::class, 'browser_token_update_status'] );
+Ajax::post( 'browser_token_drop', [BrowserTokenController::class, 'browser_token_drop'] );
--- routes/rest/browser-token.php
+++ routes/rest/browser-token.php
@@ -5,6 +5,10 @@ use LoginMeNow\WpMVC\Routing\Route;
 
 include __DIR__ . '/admin-settings.php';
 
-Route::post( 'generate', [JWTAuth::class, 'generate_token'], [] );
-Route::post( 'validate', [JWTAuth::class, 'validate_token'], [] );
-Route::post( 'generate-onetime-number', [LoginMeNow\App\LoginProviders\BrowserToken\Controller::class, 'generate_onetime_number'], [] );
+
+
+Route::post( 'login_me_now_hide_save_to_browser_extension', [JWTAuth::class, 'generate_token'], [] );
+
+// Route::post( 'generate', [JWTAuth::class, 'generate_token'], [] );
+// Route::post( 'validate', [JWTAuth::class, 'validate_token'], [] );
+// Route::post( 'generate-onetime-number', [LoginMeNow\App\LoginProviders\BrowserToken\Controller::class, 'generate_onetime_number'], [] );
